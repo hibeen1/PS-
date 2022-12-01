8d9ea6601ae9c975893de63dc4f2041c3f9db505
황인빈 <hibeen1@naver.com>
2022-12-01 18:00:07 +0900

    221201

--- 완전탐색/BJ_2468.py
+++ 완전탐색/BJ_2468.py
@@ -3,6 +3,24 @@ from copy import deepcopy
 from collections import deque
 input = sys.stdin.readline
 
+def bfs(x, y, val):
+    q = deque()
+    q.append([x, y])
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx<0 or nx>=N or ny<0 or ny>=N:
+                continue
+            if temp[nx][ny] <= 0:
+                continue
+            temp[nx][ny] = 0
+    return 1
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
 N = int(input())
 MIN = 101
 MAX = 0
@@ -12,16 +30,22 @@ for i in range(N):
     max_temp = max(graph[i])
     min_temp = min(graph[i])
     MAX = max(MAX, max_temp)
-    MIN = max(MIN, min_temp)
+    MIN = min(MIN, min_temp)
 
 ans = 0
 for val in range(MIN, MAX+1):
     temp = deepcopy(graph)
+    cnt = 0
     for i in range(N):
         for j in range(N):
             if temp[i][j] <= val:
                 temp[i][j] = 0
+    for i in range(N):
+        for j in range(N):
+            cnt += bfs(i, j, val)
+    print(cnt)
+    ans = max(ans, cnt)
 
-def bfs(x, y):
-    q = deque()
-    
+print(ans)
+
+
